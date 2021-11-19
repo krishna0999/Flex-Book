e17230c1241d739b7e668d33d8157020f9a9b81b
krishna0999 <krishnamuraree123@gmail.com>
2021-11-19 10:03:32 +0530

    Navbar added with the routing function.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey, i am Flex-Book</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/about" element={<About />}></Route>
+        <Route exact path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h2>I am about section</h2>
+        </div>
+    )
+}
+
+export default About
